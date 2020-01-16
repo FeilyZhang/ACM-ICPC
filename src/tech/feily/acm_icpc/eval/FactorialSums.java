056e006abe081ba1ddf3a88cf2fd8c2d96f6535f
Feily Zhang <fei472625@foxmail.com>
2020-01-16 11:30:30 +0800

    Given an integer, calculate its factorial sums in its range.

---
+++ src/tech/feily/acm_icpc/eval/FactorialSums.java
@@ -0,0 +1,25 @@
+package tech.feily.acm_icpc.eval;
+
+public class FactorialSums {
+
+    public static int fac(int n) {
+        int val = 1;
+        for (int i = 1; i <= n; i++) {
+            val *= i;
+        }
+        return val;
+    }
+    
+    public static int sum(int n) {
+        int val = 0;
+        for (int i = 1; i <= n; i++) {
+            val += fac(i);
+        }
+        return val;
+    }
+    
+    public static void main(String[] args) {
+        System.out.println(sum(3));
+    }
+
+}
